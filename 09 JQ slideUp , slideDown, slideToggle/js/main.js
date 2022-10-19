6c14b5193c580b99a010967119a726fd5885ce16
Pobitrodeb <pobitrodn@gmail.com>
2022-10-19 06:40:36 +0600

    Website FAQ section working using plugin , slide up and down

---
+++ 09 JQ slideUp , slideDown, slideToggle/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+   $('.botton').click(function (e) { 
+       $('p').slideToggle();
+       
+   });
+    $('.botton1').click(function (e) { 
+       $('.p1').slideToggle();
+       
+   });
+     $('.botton2').click(function (e) { 
+       $('.p2').slideToggle();
+       
+   });
+     $('.botton3').click(function (e) { 
+       $('.p3').slideToggle();
+       
+   });
+     $('.botton4').click(function (e) { 
+       $('.p4').slideToggle();
+       
+   });
+});
