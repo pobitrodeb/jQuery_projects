dadddd70ed6905be9085991e2cf0bb7006458e99
Pobitrodeb <pobitrodn@gmail.com>
2022-10-19 06:38:36 +0600

    website scroll to top plugin

---
+++ 08 jq scroll to top/js/custom.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$('body').materialScrollTop();
+	new WOW().init();
+});
---
+++ 08 jq scroll to top/js/main.js
@@ -0,0 +1,9 @@
+$('document').ready(function(){
+    $('body').materialScrollTop();
+
+
+    $('h2').counterUp({
+        delay: 20,
+        time: 5000
+    });
+});
