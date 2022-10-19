ebfbe71433d30af8f7e59ac9a7b548adb303e2ed
Pobitrodeb <pobitrodn@gmail.com>
2022-10-19 06:43:33 +0600

    Form Hide and Show plugin

---
+++ 11 JQuery hide, show ,toggle/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+      $(".click").click(function(){
+    $(".contactSubmit").toggle(500);
+  });
+  // $("#show").click(function(){
+  //   $("p").show();
+  // });
+});
